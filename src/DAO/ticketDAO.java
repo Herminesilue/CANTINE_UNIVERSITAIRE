0f377add91f2defaa0149160b4200d5ab969f061
siluehermine <silueariane8@gamail>
2022-03-19 13:52:24 +0100

    configuration

---
+++ src/DAO/ticketDAO.java
@@ -0,0 +1,5 @@
+package DAO;
+
+public class ticketDAO  implements IticketDAO{
+
+}
---
+++ src/util/hibernate.java
@@ -0,0 +1,65 @@
+package util;
+
+import java.util.Properties;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import model.Administrateur;
+import model.Boisson;
+import model.Commande;
+import model.Date;
+import model.Menu;
+import model.StockBoisson;
+import model.StockPlat;
+import model.Ticket;
+
+public class hibernate {
+	public class HibernateUtil {
+	    private SessionFactory sessionFactory;
+	    public SessionFactory getSessionFactory() {
+	        if (sessionFactory == null) {
+	            try {
+	                Configuration configuration = new Configuration();
+
+	                // Hibernate settings equivalent to hibernate.cfg.xml's properties
+	                Properties settings = new Properties();
+	                settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+	                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/cantine_universitaire?useSSL=false");//nom bd
+	                settings.put(Environment.USER, "root");
+	                settings.put(Environment.PASS, "");
+	                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+
+	                settings.put(Environment.SHOW_SQL, "true");
+
+	                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+	              // settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+	                configuration.setProperties(settings);
+
+	                configuration.addAnnotatedClass(Administrateur.class); 
+	                configuration.addAnnotatedClass(Boisson.class);//les classes
+	                configuration.addAnnotatedClass(Commande.class);
+	                configuration.addAnnotatedClass(Date.class);
+	                configuration.addAnnotatedClass(Menu.class);
+	                configuration.addAnnotatedClass(StockBoisson.class);
+	                configuration.addAnnotatedClass(StockPlat.class);
+	                configuration.addAnnotatedClass(Ticket.class);
+
+	                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+	                    .applySettings(configuration.getProperties()).build();
+
+	                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+	            } catch (Exception e) {
+	                e.printStackTrace();
+	            } 
+	        }
+	        return sessionFactory;
+	    }
+	}
+
+}
